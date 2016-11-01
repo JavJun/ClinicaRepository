00dddc3aade01e8947793a3d8b2e970c12918221
LN4735 <ln4735.4u70m474@outlook.com>
2016-11-01 06:20:11 +0000

    last

--- firstbeats/spec/spec_helper.rb
+++ firstbeats/spec/spec_helper.rb
@@ -96,4 +96,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+ def json
+      return @json if @json
+      @json = JSON.parse(response.body)
+    end
 end
